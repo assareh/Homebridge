47ef7da672fdf5363c2877605b7648f81151758d
Andy Assareh <assareh@me.com>
2021-11-01 12:56:43 -0700

    scripts

---
+++ office_packer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+""" This script powers on the specified lamp to a specified color
+    Requires a command line argument of lamp number
+    4 is Leo's Lamp
+    3 is Office
+    2 is Living Room Color """
+
+import os
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+# Read the API key
+API_KEY = os.environ['HUE_API_KEY']
+
+# Read the API URL
+API_URL = os.environ['HUE_API_URL']
+
+# Initialize other variables
+ON_PAYLOAD = "{\n \"on\": true,\n \"xy\": [\n 0.1822,\n 0.2687\n],\n \
+                \"sat\":254,\n \"bri\":254,\n \"hue\":0\n}"
+URL = API_URL + API_KEY + "/lights/3/state"
+
+
+def turn_on(url):
+    ''' turn on light
+        accepts the url of the light as parameter '''
+    requests.request("PUT", url, data=ON_PAYLOAD, verify=False)
+
+
+# initialize lights
+turn_on(URL)
