feab31acf67728a31bdc6ee3aa3bc619ff9e822f
Andy Assareh <assareh@me.com>
2022-07-23 15:47:11 -0700

    added 2nd phone

--- garagedoor_reminder.py
+++ garagedoor_reminder.py
@@ -4,7 +4,8 @@ from twilio.rest import Client
 
 
 FROM_PHONE = os.environ['TWILIO_FROM_PHONE']
-TO_PHONE = os.environ['TWILIO_TO_PHONE']
+TO_PHONE_1 = os.environ['TWILIO_TO_PHONE_1']
+TO_PHONE_2 = os.environ['TWILIO_TO_PHONE_2']
 ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
 AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
 
@@ -14,7 +15,11 @@ SMS_CLIENT = Client(ACCOUNT_SID, AUTH_TOKEN)
 
 ## Begin Twilio ------------------------------------------------------------------------ ##
 SMS_CLIENT.messages.create(body="Please close the garage door.",
-                           to=TO_PHONE,
+                           to=TO_PHONE_1,
+                           from_=FROM_PHONE)
+
+SMS_CLIENT.messages.create(body="Please close the garage door.",
+                           to=TO_PHONE_2,
                            from_=FROM_PHONE)
 ## End Twilio ------------------------------------------------------------------------ ##
 
